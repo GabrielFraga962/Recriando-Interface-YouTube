91862c0f1e6ff5304bd9b8396e05aebe0130aaa3
Gabriel S. Fraga <gabrielsouzafraga@outlook.com>
2021-06-06 23:46:18 -0300

    Aplicalção finalizada

--- interface-youtube/src/App.js
+++ interface-youtube/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider, createMuiTheme, } from '@material-ui/core';
+import Home from './Home';
+
+// const useStyles = makeStyles ({
+//   root: {
+//   },
+// })
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const theme = createMuiTheme({
+    spacing: 4,
+    palette: {
+      type: 'dark',
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#3EA6FF',
+      },
+      background: {
+        default: '#232323',
+        dark: '#181818',
+        paper: '#232323',
+      }
+    }
+  });
+
+  // const classes =useStyles();
+
+  return ( 
+  <ThemeProvider theme={theme}>
+   <Home />
+  </ThemeProvider>
   );
 }
 
